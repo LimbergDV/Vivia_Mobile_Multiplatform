3e8702fd2f41928705c93554559caf481b9ea0a7
Limberg Del Valle <limbergdelvalle@gmail.com>
2026-06-20 17:25:10 -0600

    [REFACTOR and ADD]: adding in presentation location_permissions_page, new library added called lottie and refactor the name of choose_option_page

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:vivia_mobile/features/auth/presentation/pages/choose_option.dart';
+import 'package:vivia_mobile/features/auth/presentation/pages/choose_option_page.dart';
+import 'package:vivia_mobile/features/auth/presentation/pages/location_permissions_page.dart';
 import 'package:vivia_mobile/features/auth/presentation/pages/register_page.dart';
 import 'package:vivia_mobile/shared/theme/theme.dart';
 import 'package:vivia_mobile/shared/theme/util.dart';
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
       theme: materialTheme.light(),
       darkTheme: materialTheme.dark(),
       themeMode: ThemeMode.light,
-      home: const ChooseOptionPage(),
+      home: const LocationPermissionsPage(),
     );
   }
 }
